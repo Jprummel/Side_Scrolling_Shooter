feb521dca3184f350ad503d53c95522d19f699b5
Niels <Niels97vet@hotmail.com>
2017-04-21 14:21:04 +0200

    enemy start movement

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    private Rigidbody2D _rigidBody;
+    private bool _canMove;
+
+    private float _speed;
+    [SerializeField]    private Vector2 _startPos;
+    [SerializeField]    private Vector2 _endPos;
+
+    // Use this for initialization
+    void Start () {
+        _speed = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = _speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, _endPos, step);
+    }
+}
