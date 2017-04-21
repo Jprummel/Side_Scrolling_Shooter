448775595294dea74624317626dc5549219f0892
Niels <Niels97vet@hotmail.com>
2017-04-21 15:25:00 +0200

    basic enemy movement

--- Assets/Scripts/Enemy/EnemyEntity.cs
+++ Assets/Scripts/Enemy/EnemyEntity.cs
@@ -3,12 +3,4 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyEntity : Entity {
-
-	void Start () {
-		
-	}
-	
-	void Update () {
-		
-	}
 }
--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
-    private Rigidbody2D _rigidBody;
-    private bool _canMove;
+    private Entity _enemyEntity;
 
-    private float _speed;
-    [SerializeField]    private Vector2 _startPos;
-    [SerializeField]    private Vector2 _endPos;
+    private Rigidbody2D _rigidBody;
+    private bool _switch;
+
+    [SerializeField]    private Vector3 _startPos;
+    [SerializeField]    private Vector3 _endPos;
 
     // Use this for initialization
     void Start () {
-        _speed = 1f;
+        _enemyEntity = GetComponent<Entity>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float step = _speed * Time.deltaTime;
-        transform.position = Vector2.MoveTowards(transform.position, _endPos, step);
+
+        float step = _enemyEntity.MovementSpeed * Time.deltaTime;
+
+        if (transform.position == _startPos)
+        {
+            _switch = false;
+        }
+        else if (transform.position == _endPos)
+        {
+            _switch = true;
+        }
+
+        if (!_switch)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _endPos, step);
+        }
+        else if (_switch)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _startPos, step);
+        }
     }
 }
