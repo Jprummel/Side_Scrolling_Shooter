30e0b625df06127ee525de228920c8b4658ba7f1
Jprummel <jordiprummel@live.nl>
2017-04-21 15:03:51 +0200

    Glow test

--- Assets/Scripts/EnemyEntity.cs
+++ Assets/Scripts/EnemyEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyEntity : MonoBehaviour {
+public class EnemyEntity : Entity {
 
 	void Start () {
 		
--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -3,9 +3,4 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerEntity : Entity {
-
-    private int _level;
-    private int _currentXP;
-    private int _requiredXP;
-    private int _maxRequiredXP;
 }
