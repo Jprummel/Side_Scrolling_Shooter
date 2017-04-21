38a92ae53c0b4e41c8e188ebef248c6bc2bd4064
Lorenzo Hegberg <18927@ma-web.nl>
2017-04-21 16:14:00 +0200

    player aim

---
+++ Assets/Scripts/Player/PlayerAim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AimBarrel();
+	}
+
+    void AimBarrel()
+    {
+        Vector3 mouseToScreenPos = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(mouseToScreenPos.y, mouseToScreenPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
