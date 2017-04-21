159db6fc74521a9f7772f518272dd1c4aaf5515b
Jprummel <jordiprummel@live.nl>
2017-04-21 15:32:46 +0200

    Resolution Settings

--- Assets/Scripts/Settings/ResolutionSettings.cs
+++ Assets/Scripts/Settings/ResolutionSettings.cs
@@ -1,29 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResolutionSettings : MonoBehaviour {
 
+    [SerializeField]private Dropdown _dropdown;
     private bool _isFullScreen;
     private int _resolutionWidth;
     private int _resolutionHeight;
 
-	void Start () {
-		
-	}
-
-    public void SetResolutionWidth(int width)
-    {
-        _resolutionWidth = width;
-    }
-
-    public void SetResolutionHeight(int height)
-    {
-        _resolutionHeight = height;
-    }
-
     public void ChangeResolution()
     {
+        switch(_dropdown.value){
+            
+            case 0:
+                _resolutionWidth = 1024;
+                _resolutionHeight = 768;                
+                break;
+            case 1:
+                _resolutionWidth = 1280;
+                _resolutionHeight = 720; 
+                break;
+            case 2:
+                _resolutionWidth = 1366;
+                _resolutionHeight = 768;
+                break;
+            case 3:
+                _resolutionWidth = 1600;
+                _resolutionHeight = 900;
+                break;
+            case 4:
+                _resolutionWidth = 1920;
+                _resolutionHeight = 1080;
+                break;
+        }
         Screen.SetResolution(_resolutionWidth, _resolutionHeight, _isFullScreen);
     }
 
