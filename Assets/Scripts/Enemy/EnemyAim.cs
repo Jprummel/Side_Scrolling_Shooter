a68ae333c0f8981b617056ae86549d6bfd5489fa
Niels <Niels97vet@hotmail.com>
2017-04-24 16:50:57 +0200

    Enemy Aim

--- Assets/Scripts/Enemy/EnemyAim.cs
+++ Assets/Scripts/Enemy/EnemyAim.cs
@@ -2,26 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAim : MonoBehaviour {
+public class EnemyAim : MonoBehaviour
+{
 
-    public GameObject Player;
+    [SerializeField]
+    private GameObject _player;
+
+    private SpriteRenderer _sprite;
+    private SpriteRenderer _bodySprite;
+    private float _yRotation;
+    private float min = /*-30*/-180;
+    private float max = /*55*/180;
 
     // Use this for initialization
-    void Start()
+    void Awake()
     {
-
+        _sprite = GetComponentInChildren<SpriteRenderer>();
+        _bodySprite = GetComponentInParent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Player.transform);
+        LookAtTarget();
     }
 
-    void AimBarrel()
+    void LookAtTarget()
     {
-        Vector3 mouseToScreenPos = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-        float angle = Mathf.Atan2(mouseToScreenPos.y, mouseToScreenPos.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        Vector3 playerPos = _player.transform.position;
+        float angle = Mathf.Atan2(playerPos.y, playerPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, _yRotation, Mathf.Clamp(angle, min, max));
+        if (angle > 90 || angle < -90)
+        {
+            /*min = 135f;
+            max = 200f;*/
+            FlipSprite(true);
+        }
+        else
+        {
+            FlipSprite(false);
+            /*min = -30f;
+            max = 55f;*/
+        }
+    }
+
+    void FlipSprite(bool dir)
+    {
+        _sprite.flipX = dir;
+        _sprite.flipY = dir;
+        _bodySprite.flipX = dir;
     }
 }
