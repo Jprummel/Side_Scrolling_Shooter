179d26b19ea1936c2bddf440418a9513f1dd0ef0
Jprummel <jordiprummel@live.nl>
2017-04-25 01:27:00 +0200

    Camera Follower and shakes

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
     [SerializeField]private Vector2 _velocity;
-	// Use this for initialization
+    [SerializeField]private float _smoothTimeX;
+    [SerializeField]private float _smoothTimeY;
+    [SerializeField]private GameObject _player;
+
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		
+
+        float posX = Mathf.SmoothDamp(transform.position.x, _player.transform.position.x, ref _velocity.x, _smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, _player.transform.position.y, ref _velocity.y, _smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
 	}
 }
