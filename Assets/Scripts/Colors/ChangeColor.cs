1f07f7a4efd3caa54c44f9ab10d4276005321ef8
Jprummel <jordiprummel@live.nl>
2017-04-24 09:42:05 +0200

    Color changer

---
+++ Assets/Scripts/Colors/ChangeColor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+    public void ChangeState(SpriteRenderer sprite, Color beginColor, Color endColor)
+    {
+        sprite.color = Color.Lerp(beginColor, endColor, Time.deltaTime);
+    }
+}
--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -3,4 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerEntity : Entity {
+
+    [SerializeField]private Color _fullHealthColor;
+    [SerializeField]private Color _damagedColor;
+    [SerializeField]private Color _injuredColor;
+    [SerializeField]private Color _deadColor;
 }
