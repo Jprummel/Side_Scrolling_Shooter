d0afa92ed44c13daf30f60afdddc4e0de08db398
Niels <Niels97vet@hotmail.com>
2017-04-24 16:17:01 +0200

    2

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour {
 
+    [SerializeField]private GameObject _shootPoint;
+
+    [SerializeField]private GameObject _bullet;
+
     private Entity _enemyEntity;
+
+    [SerializeField]private float _waitForNextBullet;
+
+    private float _reloadTime = 10;
+
+    [SerializeField]private float timeLeft = 3.0f;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            StartCoroutine(Shoot());
+            timeLeft = _reloadTime;
+        }
+    }
+
+
+    IEnumerator Shoot()
+    {
+        Instantiate(_bullet, _shootPoint.transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(_waitForNextBullet);
+    }
 }
