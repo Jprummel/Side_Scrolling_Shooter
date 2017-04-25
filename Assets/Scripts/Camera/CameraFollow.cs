a688765d6e01ac92ae8715b7b333f2ae45733e3d
Jprummel <jordiprummel@live.nl>
2017-04-25 15:14:51 +0200

    Small cleaning and new placeholder head

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -7,7 +7,7 @@ public class CameraFollow : MonoBehaviour {
     [SerializeField]private Vector2 _velocity;
     [SerializeField]private float _smoothTimeX;
     [SerializeField]private float _smoothTimeY;
-    [SerializeField]private GameObject _player;
+    [SerializeField]private Transform _player;
 
 	void Start () {
 		
@@ -16,8 +16,8 @@ public class CameraFollow : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
-        float posX = Mathf.SmoothDamp(transform.position.x, _player.transform.position.x, ref _velocity.x, _smoothTimeX);
-        float posY = Mathf.SmoothDamp(transform.position.y, _player.transform.position.y, ref _velocity.y, _smoothTimeY);
+        float posX = Mathf.SmoothDamp(transform.position.x, _player.position.x, ref _velocity.x, _smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, _player.position.y, ref _velocity.y, _smoothTimeY);
 
         transform.position = new Vector3(posX, posY, transform.position.z);
 	}
