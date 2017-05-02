c22a96265f8a581206d98f9d31dad8a7099b97ad
Niels <Niels97vet@hotmail.com>
2017-05-02 15:23:21 +0200

    map part 1 and part 2 update

---
+++ Assets/Scripts/Enemy/EnemyFlipSprite.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFlipSprite : MonoBehaviour {
+
+    [SerializeField]private EnemyMovement _enemyMovement;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FlipSprite();
+	}
+
+
+    private void FlipSprite()
+    {
+        if (_enemyMovement._enemySwitch == true)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
