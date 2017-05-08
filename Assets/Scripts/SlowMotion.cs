1f42352a9bdc9fdaafaced1fbc9243a93296ed89
Lorenzo Hegberg <18927@ma-web.nl>
2017-05-08 13:36:09 +0200

    added slowmotion, imported spine files

--- Assets/Scripts/SlowMotion.cs
+++ Assets/Scripts/SlowMotion.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class SlowMotion : MonoBehaviour 
 {
+    private float _normalTimescale = 1f;
+    private float _slowMoTimescale = 0.5f;
+
     private float _slowMoTimer;
     private bool _startSlowMotion;
     private bool _stopSlowMotion;
@@ -14,20 +17,29 @@ public class SlowMotion : MonoBehaviour
         if (_startSlowMotion)
         {
             _slowMoTimer += Time.fixedDeltaTime;
-            Time.timeScale = Mathf.Lerp(1, 0.5f, _slowMoTimer * 2);
+            Time.timeScale = Mathf.Lerp(_normalTimescale, _slowMoTimescale, _slowMoTimer * 2);
         }
         else if(_stopSlowMotion)
         {
             _slowMoTimer -= Time.fixedDeltaTime;
-            Time.timeScale = Mathf.Lerp(0.5f, 1, _slowMoTimer * 2);
+            Time.timeScale = Mathf.Lerp(_slowMoTimescale, _normalTimescale, _slowMoTimer * 2);
         }
     }
 
     public IEnumerator SlowMo(float waitTime)
     {
-        _startSlowMotion = true;
-        yield return new WaitForSeconds(waitTime);
-        _slowMoTimer = 0;
-        _stopSlowMotion = true;
+        if (!_isSlowMo)
+        {
+            _isSlowMo = true;
+            _startSlowMotion = true;
+            _slowMoTimer = 0;
+            yield return new WaitForSeconds(waitTime);
+            _startSlowMotion = false;
+
+            _stopSlowMotion = true;
+            yield return new WaitForSeconds(waitTime);
+            _stopSlowMotion = false;
+            _isSlowMo = false;
+        }
     }
 }
