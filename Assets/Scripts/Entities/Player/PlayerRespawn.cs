27770b021ec98224c19a218ab799b53e62decb85
Jprummel <jordiprummel@live.nl>
2017-05-09 08:53:07 +0200

    Respawn fades and routine

---
+++ Assets/Scripts/Entities/Player/PlayerRespawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour {
+
+    public delegate void RespawnEvent();
+    public static RespawnEvent respawnEvent;
+
+    public Transform RespawnPoint { get; set; }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Respawn();
+        }
+    }
+
+    public void Respawn()
+    {
+        if (respawnEvent != null)
+        {
+            respawnEvent();
+        }
+        this.transform.position = RespawnPoint.position;
+    }
+}
---
+++ Assets/Scripts/Entities/Player/RespawningRoutine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RespawningRoutine : MonoBehaviour {
+
+    [SerializeField]private Image _fadeImage;
+    [SerializeField]private float _respawnTime;
+    private Fades _fades;
+
+	void Awake () {
+        _fades = GameObject.FindGameObjectWithTag(Tags.FADEROBJECT).GetComponent<Fades>();
+        PlayerRespawn.respawnEvent += Respawn;
+	}
+
+    void Respawn()
+    {
+        StartCoroutine(RespawnRoutine());
+    }
+
+    IEnumerator RespawnRoutine()
+    {
+        //Play death animation
+        //yield return new WaitForSeconds();
+        StartCoroutine(_fades.FadeOut(_fadeImage, false, _respawnTime));
+        yield return new WaitForSeconds(_respawnTime + 0.5f);
+        StartCoroutine(_fades.FadeIn(_fadeImage));
+        //yield return new WaitForSeconds();
+        //play respawn animation
+    }
+
+    void OnDisable()
+    {
+        PlayerRespawn.respawnEvent -= Respawn;
+    }
+}
